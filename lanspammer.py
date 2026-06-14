482fe3d9d27d29a6e52deecb40aef037b5af1c86
slava240 <119606780+slava240@users.noreply.github.com>
2026-06-14 12:06:37 +0300

    added a count of spam

--- lanspammer.py
+++ lanspammer.py
@@ -2,7 +2,12 @@ import socket, time
 
 S, P, T = "224.0.2.60", 4445, 2
 
-def announce(servers, interval=1.5):
+servers = [
+    {"motd": "MiniLanSpammer", "port": 25565},
+]
+N = 10
+
+def announce(interval=1.5):
     msgs = [f"[MOTD]{s['motd']}[/MOTD][AD]{s['port']}[/AD]".encode() for s in servers]
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, T)
@@ -13,8 +18,5 @@ def announce(servers, interval=1.5):
     finally:
         sock.close()
 
-announce([
-    {"motd": "Выживание", "port": 25565},
-    {"motd": "Творческий", "port": 25566},
-    {"motd": "Мини-игры", "port": 25567},
-])
+print(f"Анонсируем {N} сервер(а/ов)")
+announce()
